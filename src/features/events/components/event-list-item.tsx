418d64aa41f33390aaff3cf53b48ccfd93164022
Dave Nguyen <dave@haidave.com>
2025-04-06 00:43:46 +0200

    feat(event-list-item): add styles to isPastEvent

--- src/features/events/components/event-list-item.tsx
+++ src/features/events/components/event-list-item.tsx
@@ -3,6 +3,8 @@ import { Button } from '@/components/ui/button'
 import { type Event } from '@/db/schema'
 import { format } from 'date-fns'
 
+import { cn } from '@/lib/utils'
+
 import { AddEventForm } from './add-event-form'
 
 type EventListItemProps = {
@@ -13,13 +15,14 @@ type EventListItemProps = {
 export function EventListItem({ event, onDelete }: EventListItemProps) {
   const [isEditDialogOpen, setIsEditDialogOpen] = useState(false)
   const eventDate = new Date(event.date)
+  const isPastEvent = eventDate < new Date()
 
   return (
     <>
       <Button variant="ghost" onClick={() => setIsEditDialogOpen(true)} className="h-max justify-start">
         <div className="grid gap-1 text-left">
           <p className="text-muted-foreground">{format(eventDate, 'do')}</p>
-          <div>
+          <div className={cn(isPastEvent && 'text-muted-foreground line-through')}>
             <span className="text-sm">{event.title}</span>
             {event.description && <span className="text-muted-foreground ml-2 text-xs">({event.description})</span>}
           </div>
